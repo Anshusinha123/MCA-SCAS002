a690b3e886ed436e8c85ee280a680b18160288c1
Anshusinha123 <anandanshu0013@gmail.com@example.com>
2024-06-11 10:19:17 +0530

    java_codenotes

---
+++ java_codenotes/first _program.java
@@ -0,0 +1,12 @@
+// java_notes_first program
+// 1.
+public class first_program
+{
+public static void main(Strings[]args)
+{
+system.out.print("Hello, welcome to java");
+}
+}
+
+//2.
+//public 
---
+++ java_codenotes/first _program.java.java
@@ -0,0 +1,11 @@
+#java_notes_first program
+1.
+public class first program
+{
+public static void main(Strings[]args)
+{system.out.print("Hello, welcome to java")
+}
+}
+
+2.
+public 
